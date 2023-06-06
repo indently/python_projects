cc36eb5c4c5748880f0e1211ab2efb11ae03a978
Federico <federico@fastmail.com>
2023-06-06 09:07:17 +0200

    Did some major refractoring to make it easier to organise projects later. And also added the cryptocurrency alerter project.

---
+++ projects_11_to_20/cryptocurrency_alerter/crypto_data.py
@@ -0,0 +1,45 @@
+import requests
+from dataclasses import dataclass
+from typing import Final
+
+BASE_URL: Final[str] = 'https://api.coingecko.com/api/v3/coins/markets'
+
+
+@dataclass
+class Coin:
+    name: str
+    symbol: str
+    current_price: float
+    high_24h: float
+    low_24h: float
+    price_change_24h: float
+    price_change_percentage_24h: float
+
+    def __str__(self):
+        return f'{self.name} ({self.symbol}): €{self.current_price:,}'
+
+
+def get_coins() -> list[Coin]:
+    """
+    Gets coins from an api and returns them as a list[Coin]
+    API: https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur
+    """
+
+    # Make the request in euros
+    payload = {'vs_currency': 'eur', 'order': 'market_cap_desc'}
+    data = requests.get(BASE_URL, params=payload)
+    json: dict = data.json()
+
+    # Create coins and add them to a coin list
+    coins_list: list[Coin] = []
+    for item in json:
+        current_coin: Coin = Coin(name=item.get('name'),
+                                  symbol=item.get('symbol'),
+                                  current_price=item.get('current_price'),
+                                  high_24h=item.get('high_24h'),
+                                  low_24h=item.get('low_24h'),
+                                  price_change_24h=item.get('price_change_24h'),
+                                  price_change_percentage_24h=item.get('price_change_percentage_24h'))
+        coins_list.append(current_coin)
+
+    return coins_list
---
+++ projects_11_to_20/cryptocurrency_alerter/main.py
@@ -0,0 +1,24 @@
+from crypto_data import get_coins, Coin
+
+
+def create_alert(symbol: str, bottom: float, top: float, coins_list: list[Coin]):
+    """Creates an alert for the given price range of a coin"""
+
+    for coin in coins_list:
+        if coin.symbol == symbol:
+            if coin.current_price > top or coin.current_price < bottom:
+                # Add the code you want to be executed if a coin reaches
+                print(coin, '!!!TRIGGER!!!')
+            else:
+                print(coin)
+
+
+if __name__ == '__main__':
+    coins: list[Coin] = get_coins()
+
+    create_alert('btc', bottom=10, top=20_000, coins_list=coins)
+    create_alert('eth', bottom=10, top=1_700, coins_list=coins)
+    create_alert('xrp', bottom=0.47, top=0.48, coins_list=coins)
+
+    # for coin in coins:
+    #     print(coin)
