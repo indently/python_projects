9f703e81cf90ee03f5f832b459a59aa895545265
Federico <federico@fastmail.com>
2023-06-17 23:06:47 +0200

    Added common password project

---
+++ starter_projects/common_password/main.py
@@ -0,0 +1,25 @@
+def check_password(password: str):
+    """Checks whether a password is in the 100.000 most common passwords."""
+
+    # Open up the file with most common passwords
+    with open('passwords.text', 'r') as file:
+        common_passwords: list[str] = file.read().splitlines()
+
+    # If any of the words are equal to the password, return True
+    for i, common_password in enumerate(common_passwords, start=1):
+        if password == common_password:
+            print(f'{password}: ❌ (#{i})')
+            return  # Exit the function as soon as we have a match
+
+    # If the password is not located in the common passwords, it's unique
+    print(f'{password}: ✅ (Unique)')
+
+
+def main():
+    # Check password
+    user_password: str = input('Enter a password: ')
+    check_password(password=user_password)
+
+
+if __name__ == '__main__':
+    main()
